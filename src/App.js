5c86bc47385c93dbdf1f22ac308c26c3da9f94a4
eraybulut34 <abdullaheraybulut@hotmail.com>
2022-04-06 00:37:33 +0300

    create the todo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+const INITIAL_STATE = [
+  { id: 1, baslik: "Yapılacak 1", done: false },
+  { id: 2, baslik: "Yapılacak 2", done: true }
+];
 
 function App() {
+  const [list, setList] = useState(INITIAL_STATE);
+const [newTodo, setNewTodo] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <b-table></b-table>
+      <h1>Yapılacaklar Listesi</h1>
+      <div className="add-form">
+        <input value={newTodo}
+        autoFocus
+        type="text" placeholder="Yapılacakları Giriniz" 
+        onChange={
+          (e) => {
+            setNewTodo(e.target.value);
+          }
+        }
+        />
+        <button 
+        //set the enter
+
+        class="btn btn-primary" onClick={
+          () => {
+            setList([...list, { id: list.length + 1, baslik: newTodo, done: false }]);
+            setNewTodo('');
+          }
+        }>Ekle</button>
+      </div>
+      <div className="list">
+        
+        {list.map(item => (
+          <div key={item.id}  onClick={() =>
+            setList(list.map(item2 => item2.id === item.id ? { ...item, done: !item.done } : item2))
+          } className={item.done ? "done" : ""}>{item.baslik}</div>))}
+      </div>
+      <button class="btn btn-primary" onClick={
+        () => setList(list.filter(item => !item.done))
+      }>Tamamlananları Temizle</button>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import './Navbar.css';
+
+function App() {
+  return (
+    <div>
+  <div className="hello">
+     
+  </div>
+
+    </div>
+  );
+}
+
+export default App;
