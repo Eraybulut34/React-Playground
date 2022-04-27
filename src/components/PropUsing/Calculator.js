b82fbc1e3910a82f7192173acbe406da27e01d1e
eraybulut34 <abdullaheraybulut@hotmail.com>
2022-04-27 10:37:04 +0300

    add celcius calculator

---
+++ src/components/PropUsing/Calculator.js
@@ -0,0 +1,28 @@
+import React, { useState, Component } from 'react';
+import BoilingVerdict from './BoilingVerdict';
+
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = { temperature: '555', eray: '5454544' };
+    }
+    handleChange(e) {
+        this.setState({ temperature: e.target.value });
+    }
+    render() {
+        const temperature = this.state.temperature;
+        return (
+            <fieldset>
+                <legend>Enter temperature in Celsius:{this.state.eray}</legend>
+                <input
+                    value={temperature}
+                    onChange={this.handleChange} />
+                <BoilingVerdict
+                    geldi={parseFloat(temperature)} />
+
+            </fieldset>
+        );
+    }
+}
+export default Calculator;
---
+++ src/components/Todos/Todos.js
@@ -0,0 +1,51 @@
+import './Todos.css';
+import React, { useState } from 'react';
+
+const INITIAL_STATE = [
+  { id: 1, baslik: "Yapılacak 1", done: false },
+  { id: 2, baslik: "Yapılacak 2", done: true }
+];
+
+function Todos() {
+  const [list, setList] = useState(INITIAL_STATE);
+const [newTodo, setNewTodo] = useState('');
+
+  return (
+    <div className="App">
+      <b-table></b-table>
+      <h1>Yapılacaklar Listesi</h1>
+      <div className="add-form">
+        <input value={newTodo}
+        autoFocus
+        type="text" placeholder="Yapılacakları Giriniz" 
+        onChange={
+          (e) => {
+            setNewTodo(e.target.value);
+          }
+        }
+        />
+        <button 
+        //set the enter
+
+        class="btn btn-primary" onClick={
+          () => {
+            setList([...list, { id: list.length + 1, baslik: newTodo, done: false }]);
+            setNewTodo('');
+          }
+        }>Ekle</button>
+      </div>
+      <div className="list">
+        
+        {list.map(item => (
+          <div key={item.id}  onClick={() =>
+            setList(list.map(item2 => item2.id === item.id ? { ...item, done: !item.done } : item2))
+          } className={item.done ? "done" : ""}>{item.baslik}</div>))}
+      </div>
+      <button class="btn btn-primary" onClick={
+        () => setList(list.filter(item => !item.done))
+      }>Tamamlananları Temizle</button>
+    </div>
+  );
+}
+
+export default Todos;
